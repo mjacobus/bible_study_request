8c7e5c757f8e7e0748e9704b884f59e1a54e3ecd
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-03-24 12:36:15 -0300

    Add root redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: ->(env) { [200, {}, ['ok']] }
+  root to: redirect('estudo-biblico?cid=root')
 
   get 'estudo-biblico', to: 'bible_study_requests#new', as: :new_bible_study_request
   post 'estudo-biblico', to: 'bible_study_requests#create'
